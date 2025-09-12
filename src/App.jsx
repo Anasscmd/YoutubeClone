fdb6897ed38ec97529248da4fd5810d25fceacfe
Anasscmd <anasssessalmanyy@gmail.com>
2025-09-12 20:06:13 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+import { Route, Routes } from 'react-router-dom'
 
+
+const [sidebar , setSidebar] = React.useState(true);
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar setSidebar={setSidebar} />
+      <Routes>
+        <Route path='/' element={<Home  sidebar={sidebar}  />}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video />}/>
+      </Routes>
     </div>
   )
 }
